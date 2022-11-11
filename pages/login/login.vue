d655ff5632f97a5b0a288b050f8bc8838b189e28
黄浩程 <18373075@buaa.edu.cn>
2022-11-11 17:54:40 +0800

    frame

---
+++ pages/login/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <view class="uni-container">
+        <a>登录页面</a>
+        <button class="" style="margin-top: 10rpx;" @click="bindVisit()">
+            只是个登录跳转
+        </button>
+    </view>
+</template>
+
+<script>
+    export default{
+        methods: {
+            bindVisit() {
+                uni.switchTab({
+                    url: '../main/home'
+                })
+            }
+        }
+    }
+    
+</script>
+
+<style>
+</style>
