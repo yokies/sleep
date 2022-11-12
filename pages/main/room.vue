207e0991fcbef961b215e062d738887d0d8f9cfb
黄浩程 <18373075@buaa.edu.cn>
2022-11-12 17:25:10 +0800

    all background

--- pages/main/room.vue
+++ pages/main/room.vue
@@ -1,8 +1,42 @@
 <template>
+    <view class='room-container'>
+        <image class="room-image" src="../../static/room/room-background.png">
+            
+        </image>
+        <button type="primary" class="room-button" @click="visit_room">
+            进入房间
+        </button>   
+    </view>
 </template>
 
 <script>
+    export default {
+        methods: {
+            visit_room() {
+                uni.navigateTo({
+                    url: "../room/sleeproom"
+                })
+            }
+        }
+    }
 </script>
 
 <style>
+    .room-container{
+        
+    },
+    .room-image{
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        z-index: -1;
+    },
+    .room-button{
+        position: absolute;
+        bottom: 10%;
+        left: 15%;
+        width: 70%;
+    }
 </style>
---
+++ pages/room/sleeproom.vue
@@ -0,0 +1,21 @@
+<template>
+    <view class='room-container'>
+        <image class="room-image" src="../../static/room/inroom-background.png">
+            
+        </image>
+    </view>
+</template>
+
+<script>
+</script>
+
+<style>
+    .room-image{
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        z-index: -1;
+    }
+</style>
